ee669da4425d427c76e38789b7770d25967350e2
Leonel Espinall <alliancellc323@gmail.com>
2020-03-04 00:00:00 +0300

    Collins develop (#6)
    * updated typos in  rental app's urls.py
    
    * udated the frontend by adding. (#5)
    
    - authentication pages such as login and signup.
     - Removed unnecessary js code.
     - Adde a homepage .
     - installed redux and axios

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -20,11 +20,6 @@ const quickLinks = [
     display: "Car Listing",
   },
   {
-    path: "/blogs",
-    display: "Blog",
-  },
-
-  {
     path: "/contact",
     display: "Contact",
   },
@@ -43,7 +38,7 @@ const Footer = () => {
                 <Link to="/home" className=" d-flex align-items-center gap-2">
                   <i class="ri-car-line"></i>
                   <span>
-                    Rent Car <br /> Service
+                  Book Ride or Rent Car<br /> Service
                   </span>
                 </Link>
               </h1>
@@ -72,12 +67,12 @@ const Footer = () => {
           <Col lg="3" md="4" sm="6">
             <div className="mb-4">
               <h5 className="footer__link-title mb-4">Head Office</h5>
-              <p className="office__info">123 Zindabazar, Sylhet, Bangladesh</p>
-              <p className="office__info">Phone: +0995345875365</p>
+              <p className="office__info">123 Street</p>
+              <p className="office__info">Phone: +00</p>
 
-              <p className="office__info">Email: muhib5532@gmail.com</p>
+              <p className="office__info">Email: @gmail.com</p>
 
-              <p className="office__info">Office Time: 10am - 7pm</p>
+              <p className="office__info">Office Time: 10am - 4pm</p>
             </div>
           </Col>
 
@@ -98,7 +93,7 @@ const Footer = () => {
             <div className="footer__bottom">
               <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
                 <i class="ri-copyright-line"></i>Copyright {year}, Developed by
-                Muhibur Rahman. All rights reserved.
+                I  :) . All rights reserved.
               </p>
             </div>
           </Col>
--- frontend/src/pages/Rental.jsx
+++ frontend/src/pages/Rental.jsx
@@ -9,12 +9,9 @@ import AboutSection from "../components/UI/AboutSection";
 import ServicesList from "../components/UI/ServicesList";
 import carData from "../assets/data/carData";
 import CarItem from "../components/UI/CarItem";
-import BecomeDriverSection from "../components/UI/BecomeDriverSection";
 import Testimonial from "../components/UI/Testimonial";
 
-import BlogList from "../components/UI/BlogList";
-
-const Home = () => {
+const Rental = () => {
   return (
     <Helmet title="Home">
       {/* ============= hero section =========== */}
@@ -68,7 +65,7 @@ const Home = () => {
         </Container>
       </section>
       {/* =========== become a driver section ============ */}
-      <BecomeDriverSection />
+      {/* <BecomeDriverSection /> */}
 
       {/* =========== testimonial section =========== */}
       <section>
@@ -88,12 +85,12 @@ const Home = () => {
       <section>
         <Container>
           <Row>
-            <Col lg="12" className="mb-5 text-center">
+            {/* <Col lg="12" className="mb-5 text-center">
               <h6 className="section__subtitle">Explore our blogs</h6>
               <h2 className="section__title">Latest Blogs</h2>
             </Col>
 
-            <BlogList />
+            <BlogList /> */}
           </Row>
         </Container>
       </section>
@@ -101,4 +98,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Rental;
