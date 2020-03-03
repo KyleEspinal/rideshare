d399872742682743f153558333a0aa51b3302943
Leonel Espinall <alliancellc323@gmail.com>
2020-03-04 00:00:00 +0300

    updated typos in  rental app's urls.py

--- server/rental/urls.py
+++ server/rental/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from .views import RentalView
 
-app_name = 'taxi'
+app_name = 'cabs'
 
 urlpatterns = [
     path('', RentalView.as_view({'get': 'list'}), name='rental_list'),
-    path('<uuid:trip_id>/', RentalView.as_view({'get': 'retrieve'}), name='rental_detail'),  
+    path('<uuid:rental_id>/', RentalView.as_view({'get': 'retrieve'}), name='rental_detail'),  
 ]
