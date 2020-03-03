de6d684897810e1be1eef94a8a8d98e9c6dcb99f
Leonel Espinall <alliancellc323@gmail.com>
2020-03-04 00:00:00 +0300

    Commits show frontend update, email backend and backend update  (#7)
    * udated the frontend by adding.
     - authentication pages such as login and signup.
     - Removed unnecessary js code.
     - Adde a homepage .
     - installed redux and axios
    
    * Added Car model choice filed to Rental model.
    
    * Made a new Email model in rental app .
     - configured the path for the models view inorder
     to send emails
    Added the EMAIL details to settings.py
    Registerd the email model to the admin.

--- server/cabs/settings.py
+++ server/cabs/settings.py
@@ -185,3 +185,11 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
     'USER_ID_CLAIM': 'id',
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
